0d4972039bde46bddb24e36f773d94979a1691ad
Balazs Bellus <balazs.bellus@jaystack.com>
2023-02-06 13:21:36 +0100

    Project Base

---
+++ src/LogService2023.App/LogService2023.App/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace LogService2023.App
+{
+    public class Settings
+    {
+        public string DbConnectionString { get; }
+
+        public Settings(IConfiguration config)
+        {
+            DbConnectionString = config["ConnectionStrings:DatabaseConnection"];
+        }
+    }
+}
---
+++ src/LogService2023.App/LogService2023.App/Startup.cs
@@ -0,0 +1,55 @@
+﻿namespace LogService2023.App
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private readonly Settings settings;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            settings = new Settings(Configuration);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(p => settings);
+
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
